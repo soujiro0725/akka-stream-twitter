2b92c2400ac1d65c756e28157cbcec0f69c1d070
sishida <sishida@gsx.co.jp>
2018-03-28 08:59:44 +0900

    flow incomplete

---
+++ src/main/scala/soujiro0725/twitter/FlowsComponent.scala
@@ -0,0 +1,9 @@
+package com.soujiro0725.twitter
+
+trait FlowsComponent {
+  val flows: FlowsComponent
+
+  class Flows {
+
+  }
+}
--- src/main/scala/soujiro0725/twitter/TwitterApp.scala
+++ src/main/scala/soujiro0725/twitter/TwitterApp.scala
@@ -1,5 +1,12 @@
 package com.soujiro0725.twitter
 
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Flow, GraphDSL, RunnableGraph, Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy}
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 trait TwitterApp {
 
   this: TwitterService =>
@@ -7,11 +14,16 @@ trait TwitterApp {
   val app: TwitterStream
 
   class TwitterStream {
+
+    implicit val actorSystem = ActorSystem("twitter-app")
+    implicit val materializer = ActorMaterializer()
+    private[this] val logger = Logger[TwitterStream]
+
     def execute(): Unit = {
       println("running twitter app ...")
 
-      // config.mode match
-      // TODO
+      val source = Source.queue[Status](8, OverflowStrategy.backpressure)
+
       twitterAPI.streamByTrack(Seq("nba", "nytimes"), new StatusListenerImpl())
     }
   }
