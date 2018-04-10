5ab5d036d72b0dea06fd6723cd4147ee9f82d062
sishida <sishida@gsx.co.jp>
2018-04-11 08:27:21 +0900

    sentiment analysis flow added

--- src/main/scala/soujiro0725/twitter/TwitterApp.scala
+++ src/main/scala/soujiro0725/twitter/TwitterApp.scala
@@ -10,6 +10,7 @@ import twitter4j.{User, Status}
 import com.soujiro0725.twitter.SentimentAnalysisUtils._
 
 case class Tweet(statusId: Long, user: User, uris: Iterable[String])
+case class TweetSentiment(sentimentType: SENTIMENT_TYPE)
 
 trait TwitterApp {
 
@@ -21,21 +22,18 @@ trait TwitterApp {
 
     implicit val actorSystem = ActorSystem("twitter-app")
     implicit val materializer = ActorMaterializer()
-    private[this] val logger = Logger[TwitterStream]
 
     def execute(): Unit = {
       println("running twitter app ...")
 
       val source = Source.queue[Status](8, OverflowStrategy.backpressure)
-      val flow = Flow.fromFunction((s: Status) => {
-        val uris = s.getExtendedMediaEntities.map { me => me.getMediaURLHttps }.toSeq
-        Tweet(s.getId, s.getUser, uris)
-      })
-      val sink = Sink.foreach[Tweet] { mt =>
-        logger.info(s"https://twitter.com/${mt.user.getScreenName}/status/${mt.statusId}, ${mt.uris}")
-      }
+      val flows = new Flows
+      val sinks = new Sinks
 
-      val streamQueue = source.via(flow).to(sink).run()
+      val streamQueue = source
+        .via(flows.analyzeSentiment)
+        .to(sinks.sink2)
+        .run()
 
       def onStatus(status: Status): Unit = {
         streamQueue.offer(status)
@@ -44,6 +42,31 @@ trait TwitterApp {
       twitterAPI.streamByTrack(Seq("nba", "nytimes"), new StatusListenerImpl(Some(onStatus)))
 
       Await.result(actorSystem.whenTerminated, Duration.Inf)
+    }
+  }
+
+  class Flows {
+
+    val func1 = Flow.fromFunction((s: Status) => {
+      val uris = s.getExtendedMediaEntities.map { me => me.getMediaURLHttps }.toSeq
+      Tweet(s.getId, s.getUser, uris)
+    })
+
+    val analyzeSentiment = Flow.fromFunction((s: Status) => {
+      val st = detectSentiment(s.getText)
+      TweetSentiment(st)
+    })
+  }
+
+  class Sinks {
+    private[this] val logger = Logger[TwitterStream]
+
+    val sink1 = Sink.foreach[Tweet] { mt =>
+      logger.info(s"https://twitter.com/${mt.user.getScreenName}/status/${mt.statusId}, ${mt.uris}")
+    }
+
+    val sink2 = Sink.foreach[TweetSentiment] { mt =>
+      logger.info(s"${mt.sentimentType.toString}")
     }
   }
 }
