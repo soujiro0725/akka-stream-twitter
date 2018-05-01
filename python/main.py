f9b530c3217c20ff9f73f248ad7bd7c04c8cdaeb
sishida <sishida@gsx.co.jp>
2018-05-01 10:04:42 +0900

    range slider updated

--- python/main.py
+++ python/main.py
@@ -5,6 +5,7 @@ import json
 from boto3.dynamodb.conditions import Key, Attr
 import datetime
 from datetime import timedelta
+from dateutil.relativedelta import relativedelta
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -16,31 +17,56 @@ dynamodb = boto3.client('dynamodb', endpoint_url='http://localhost:7777')
 
 
 app = dash.Dash()
+
 dateparse = lambda dates : pd.datetime(dates, '%Y%m%dT%H:%M')
 df = pd.read_csv('./data/test.csv', parse_dates=['datetime'])
 
+epoch = datetime.datetime.utcfromtimestamp(0)
+def unix_time_millis(dt):
+    return (dt - epoch).total_seconds() #* 1000.0
+
+def get_marks_from_start_end(start, end):
+    ''' Returns dict with one item per month
+    {1440080188.1900003: '2015-08',
+    '''
+    result = []
+    current = start
+    while current <= end:
+        result.append(current)
+        current += relativedelta(months=1)
+    return {unix_time_millis(m):(str(m.strftime('%Y-%m'))) for m in result}
+
+min=unix_time_millis(df['datetime'].min())
+max=unix_time_millis(df['datetime'].max())
 
 app.layout = html.Div([
     dcc.Graph(id='indicator-graphic'),
 
-    dcc.Slider(
+    dcc.RangeSlider(
         id='datetime--slider',
-        min=df['datetime'].min(),
-        max=df['datetime'].max(),
-        value=df['datetime'].max(),
-        step=None, #timedelta(minutes=1),
-        marks={str(dt): str(dt) for dt in df['datetime'].unique()}
-    )
+        min=min,
+        max=max,
+        value=[min, max],
+        marks=get_marks_from_start_end(df['datetime'].min(), df['datetime'].max()),
+    ),
+    html.Div(id='rangeslider-output')
 ])
+
+@app.callback(
+    dash.dependencies.Output('rangeslider-output', 'children'),
+    [dash.dependencies.Input('datetime--slider', 'value')])
+def update_output(value):
+    return 'time range is  "{} - {}"'.format(datetime.datetime.fromtimestamp(value[0]),
+                                             datetime.datetime.fromtimestamp(value[1]))
 
 @app.callback(
     dash.dependencies.Output('indicator-graphic', 'figure'),
     [dash.dependencies.Input('datetime--slider', 'value')])
-def update_graph(datetime_value):
-    dff = df[df['datetime'] == datetime_value]
-
-    print("printing logs...")
-    print(dff)
+def update_graph(datetime_value_list):
+    min = datetime.datetime.fromtimestamp(datetime_value_list[0])
+    max = datetime.datetime.fromtimestamp(datetime_value_list[1])
+    dff = df[(min <= df['datetime']) & (df['datetime'] <= max)]
+    
     return {
         'data': [go.Scatter(
             x=dff['statusId'],
